a218b824f9db3f7a1f81256897c41550ad420948
rudaks <rudaks94@gmail.com>
2024-05-25 20:38:06 +0900

    update

--- code-interpreter/main.py
+++ code-interpreter/main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
-from langchain.agents import AgentType
+from langchain.agents import AgentType, AgentExecutor, create_react_agent
 from langchain_experimental.agents import create_csv_agent
+from langchain import hub
+from langchain_core.tools import Tool
 
 # from langchain.agents.agent_toolkits.python import create_python_agent
 from langchain_experimental.agents.agent_toolkits import create_python_agent
@@ -17,27 +19,77 @@ load_dotenv()
 
 def main():
     print("Start...")
-    python_agent_executor = create_python_agent(
-        llm=ChatOpenAI(temperature=0, model="gpt-4"),
-        tool=PythonREPLTool(),
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True,
+
+    instructions = """You are an agent designed to write and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    You have qrcode package installed
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question. 
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+        """
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+
+    tools = [PythonREPLTool()]
+    python_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools,
     )
 
-    # python_agent_executor.run(
-    #     """generate and save in current working directory 15 QRcodes
-    #                             that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
-    # )
+    python_agent_executor = AgentExecutor(agent=python_agent, tools=tools, verbose=True)
 
-    csv_agent = create_csv_agent(
+    csv_agent_executor: AgentExecutor = create_csv_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4"),
         path="episode_info.csv",
         verbose=True,
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     )
 
-    csv_agent.run("how many columns are there in file episode_info.csv")
-    csv_agent.run("print seasons ascending order of the number of episodes they have")
+    ################################ Router Grand Agent ########################################################
+
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        return python_agent_executor.invoke({"input": original_prompt})
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor_wrapper,
+            description="""useful when you need to transform natural language to python and execute the python code,
+                          returning the results of the code execution
+                          DOES NOT ACCEPT CODE AS INPUT""",
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent_executor.invoke,
+            description="""useful when you need to answer question over episode_info.csv file,
+                         takes an input the entire question and returns the answer after running pandas calculations""",
+        ),
+    ]
+
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools,
+    )
+    grand_agent_executor = AgentExecutor(agent=grand_agent, tools=tools, verbose=True)
+
+    print(
+        grand_agent_executor.invoke(
+            {
+                "input": "which season has the most episodes?",
+            }
+        )
+    )
+
+    print(
+        grand_agent_executor.invoke(
+            {
+                "input": "Generate and save in current working directory 15 qrcodes that point to `www.udemy.com/course/langchain`",
+            }
+        )
+    )
 
 
 if __name__ == "__main__":
